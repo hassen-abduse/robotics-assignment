b1b3571233e35d848995502b637b8c79ce9c95b1
hassen-abduse <hassen.abduse@gmail.com>
2021-05-23 15:33:12 +0300

    updated the arm_lib package

--- src/arm_lib/src/transformation_client.cpp
+++ src/arm_lib/src/transformation_client.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "transfomation_client");
-    if(argc != 8)
+    if (argc != 8)
     {
         ROS_INFO("usage: transformation_client X Y Z a b g d");
         return 1;
@@ -16,26 +16,28 @@ int main(int argc, char **argv)
 
     arm_lib::Transform srv;
 
-    std::vector<double> vectors = { atof(argv[1]), atof(argv[2]), atof(argv[3]) };
-    std::vector<double> angles = { atof(argv[4]), atof(argv[5]), atof(argv[6]) };
+    std::vector<double> vectors = {atof(argv[1]), atof(argv[2]), atof(argv[3])};
+    std::vector<double> angles = {atof(argv[4]), atof(argv[5]), atof(argv[6])};
     double d = atof(argv[7]);
-    
+
     srv.request.input_vectors = vectors;
     srv.request.angles = angles;
     srv.request.d = d;
-    for (int i=0; i<3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         ROS_INFO("Input Vectors: [v%u: %f]", i, srv.request.input_vectors[i]);
     }
-    for (int i=0; i<3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         ROS_INFO("Input Angles: [v%u: %f]", i, srv.request.angles[i]);
     }
     ROS_INFO("Input d: %f", srv.request.d);
-    if(client.call(srv))
+    if (client.call(srv))
     {
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < 3; i++)
+        {
             ROS_INFO("Output Vectors: [v%u: %f]", i, srv.response.output_vectors[i]);
-    }
-        
+        }
     }
 
     else
@@ -45,6 +47,4 @@ int main(int argc, char **argv)
     }
 
     return 0;
-
-
 }
