7475b06a18a30caefab7c8691e06e0275800f084
hassen-abduse <hassen.abduse@gmail.com>
2021-05-26 13:29:10 +0300

    updated angles_subscriber

--- src/arm_lib/src/angles_subscriber.cpp
+++ src/arm_lib/src/angles_subscriber.cpp
@@ -3,15 +3,15 @@
 
 void on_angle_publish(const arm_lib::angles &angles)
 {
-    ROS_INFO("Current Angle Values: \nChasis-Arm1-Z: [%f] \nChasis-Arm1-X: [%f]\nArm1-Arm2-X: [%f] \nArm2-Arm3-X: [%f] \nArm3-Arm4-X: [%f]",
-             angles.joint1_z, angles.joint1_x, angles.joint2_x, angles.joint3_x, angles.joint4_x);
+    ROS_INFO("Current Angle Values: \nChasis-Arm1: [%f] \nArm1-Arm2: [%f] \nArm2-Arm3: [%f] \nArm3-Arm4: [%f]",
+             angles.joint1, angles.joint2, angles.joint3, angles.joint4);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "angles_subscriber");
     ros::NodeHandle node;
-    ros::Subscriber sub = node.subscribe("current_joint_angles", 1000, on_angle_publish);
+    ros::Subscriber sub = node.subscribe("arm/current_joint_angles", 1000, on_angle_publish);
 
     ros::spin();
     return 0;
